669e81fa3f47d400ca1925d27b045025099b0624
Perdix of Mars <perdix.p76l@gmail.com>
2026-08-22 16:28:21 +0000

    Expose normalized mouse event fields in streams

--- src/mouse.cpp
+++ src/mouse.cpp
@@ -1,8 +1,85 @@
 #include "terminalpp/mouse.hpp"
 
 #include <iostream>
+#include <utility>
 
 namespace terminalpp::mouse {
+
+namespace {
+
+std::ostream &operator<<(std::ostream &out, button const &btn)
+{
+    switch (btn)
+    {
+        case button::none:
+            return out << "none";
+        case button::left:
+            return out << "left";
+        case button::middle:
+            return out << "middle";
+        case button::right:
+            return out << "right";
+        case button::unknown:
+            return out << "unknown";
+        case button::scrollwheel_up:
+            return out << "scrollwheel-up";
+        case button::scrollwheel_down:
+            return out << "scrollwheel-down";
+        case button::scrollwheel_right:
+            return out << "scrollwheel-right";
+        case button::scrollwheel_left:
+            return out << "scrollwheel-left";
+        case button::button_8:
+            return out << "button-8";
+        case button::button_9:
+            return out << "button-9";
+        case button::button_10:
+            return out << "button-10";
+        case button::button_11:
+            return out << "button-11";
+        default:
+            return out << "unknown";
+    }
+}
+
+void output_pipe(std::ostream &out, bool &pipe)
+{
+    if (std::exchange(pipe, true))
+    {
+        out << "|";
+    }
+}
+
+std::ostream &output_modifiers(
+    std::ostream &out, terminalpp::vk_modifier const &modifiers)
+{
+    bool pipe = false;
+
+    if ((modifiers & terminalpp::vk_modifier::shift)
+        == terminalpp::vk_modifier::shift)
+    {
+        output_pipe(out, pipe);
+        out << "shift";
+    }
+
+    if ((modifiers & terminalpp::vk_modifier::ctrl)
+        == terminalpp::vk_modifier::ctrl)
+    {
+        output_pipe(out, pipe);
+        out << "ctrl";
+    }
+
+    if ((modifiers & terminalpp::vk_modifier::meta)
+        == terminalpp::vk_modifier::meta)
+    {
+        output_pipe(out, pipe);
+        out << "meta";
+    }
+
+    return out;
+}
+
+}  // namespace
 
 // ==========================================================================
 // OPERATOR<<
@@ -37,6 +114,32 @@ std::ostream &operator<<(std::ostream &out, event const &ev)
         default:
             out << "unk";
             break;
+    }
+
+    if (ev.button_ != button::none)
+    {
+        out << ", button:" << ev.button_;
+    }
+
+    if (ev.button_code_)
+    {
+        out << ", code:" << *ev.button_code_;
+    }
+
+    if (ev.modifiers_ != terminalpp::vk_modifier::none)
+    {
+        out << ", mods:";
+        output_modifiers(out, ev.modifiers_);
+    }
+
+    if (ev.is_motion_)
+    {
+        out << ", motion";
+    }
+
+    if (ev.is_release_)
+    {
+        out << ", release";
     }
 
     return out << "]";
